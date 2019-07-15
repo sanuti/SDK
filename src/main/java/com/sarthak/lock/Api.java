6d5ff76a1cd75912ab0ec4f5d8c09dc912da0adc
User <jain.sarthak619@gmail.com>
2019-07-15 13:15:59 +0530

    Intial Commit

--- src/main/java/com/sarthak/lock/Api.java
+++ src/main/java/com/sarthak/lock/Api.java
@@ -1,10 +1,12 @@
 package com.sarthak.lock;
 
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Api {
+    @FormUrlEncoded
     @GET("lock_update.php")
     Call<String> update_lock(@Query("lock") String lock, @Query("status")String status );
 }
